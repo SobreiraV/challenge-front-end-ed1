ec47bd8a835eede4f2a5f1bb78689ae4b06a1c77
SobreiraV <sobreirav@gmail.com>
2022-11-06 12:29:54 -0300

    Salva e carrega dados e gera as miniaturas

---
+++ assets/js/carregar.js
@@ -0,0 +1,30 @@
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+const lista = document.getElementById("cards_list")
+console.log(itens)
+
+for (let i = 0; i < itens.length; i++) {
+    const item = itens[i]
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `
+        <div class="card-body">
+            <h5 class="card-nome">${item.nome}</h5>
+            <p class="card-descricao">${item.descricao}</p>
+            <div class="card-rodape">
+                <div class="card-social">
+                    <button class="card-comentarios botaosocial">9</button>
+                    <button class="card-likes botaosocial">9</button>
+                </div>
+                <img src="#" class="card-autor"/>
+            </div>
+        </div>
+    `
+    lista.appendChild(card)
+}
+
+/*
+notas para o futuro:
+1-fazer os comentarios e os likes serem interativos
+2-implementar o usuario
+*/
+
---
+++ assets/js/salvar.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("form_salvar")
+const itens = JSON.parse(localStorage.getItem("itens")) || []
+
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['opcao_campo-nome'].value
+    const descricao = evento.target.elements['opcao_campo-descricao'].value
+    const cor = evento.target.elements['linguagem'].value
+    const linguagem = evento.target.elements['opcoes_campo-cor'].value
+
+    const existe = itens.find(elemento => elemento.nome === nome)
+
+    if (existe) {
+        alert("Já existe um item com esse nome!")
+        return
+    }
+
+    evento.target.elements['opcao_campo-nome'].value = ""
+    evento.target.elements['opcao_campo-descricao'].value = ""
+
+    armazenaLocalmente(nome, descricao, cor, linguagem)
+})
+
+function armazenaLocalmente(nome, descricao, cor, linguagem) {
+    /* const itens = JSON.parse(localStorage.getItem("itens")) || [] */
+    itens.push({
+        nome,
+        descricao,
+        cor,
+        linguagem
+    })
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+}
